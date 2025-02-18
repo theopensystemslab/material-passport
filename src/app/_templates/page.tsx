e59fd7be3937af08d9c42bb833e791e802d19484
Dan G <freemvmt@posteo.net>
2025-02-18 22:02:17 +0000

    clean up tree, minor edits

--- src/app/_templates/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page({
-  // params,
-  // searchParams,
-}: {
-  params: Promise<{ slug: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-  return <h1>A PAGE</h1>
-}
