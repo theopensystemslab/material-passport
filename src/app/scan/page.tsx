86e57a6e298aec347e6947094d2710a5b67a8a7b
Dan G <freemvmt@posteo.net>
2025-03-16 17:07:13 +0000

    add QR scanner at /scan and test

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,9 +1,68 @@
-export default function Scanner({
-  // params,
-  // searchParams,
-}: {
-  params: Promise<object>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-  return <h1>SCAN QR CODE</h1>
+'use client'
+
+import { type IDetectedBarcode, Scanner} from '@yudiel/react-qr-scanner'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+const COMPONENT_UID_REGEX = /[a-z]{3}-\d{6}/i
+// we allow any junk (e.g. query string) to be tagged onto the end of these URLs - validating the domain is sufficient
+const PASSPORT_URL_PRODUCTION_REGEX = new RegExp(
+  `^(?:https?:\/\/)?(?:www\.)?wikihouse\.materialpassport\.info\/passport\/${COMPONENT_UID_REGEX.source}`, 'i')
+const PASSPORT_URL_PREVIEW_REGEX = new RegExp(
+  `^(?:https?:\/\/)?(?:www\.)?material-passport[a-z\-]+\.vercel\.app\/passport\/${COMPONENT_UID_REGEX.source}`, 'i')
+const PASSPORT_URL_LOCAL_REGEX = new RegExp(
+  `^(?:https?:\/\/)?(?:www\.)?localhost:\d{4}\/passport\/${COMPONENT_UID_REGEX.source}`, 'i')
+
+const validatePassportUrl = (url: string): boolean => {
+  return PASSPORT_URL_PRODUCTION_REGEX.test(url)
+    || PASSPORT_URL_PREVIEW_REGEX.test(url)
+    || PASSPORT_URL_LOCAL_REGEX.test(url)
 }
+
+const extractUidFromUrl = (url: string): string | null => {
+  const match = url.match(COMPONENT_UID_REGEX)
+  if (match?.[0]) {
+    console.debug(`Extracted UID: ${match[0]}`)
+    return match[0]
+  }
+  console.debug(`Failed to extract UID from URL: ${url}`)
+  return null
+}
+
+export default function Page() {
+  const router = useRouter()
+  const [feedback, setFeedback] = useState('Scan the QR code on your component')
+
+  return (<div className="flex flex-col flex-grow justify-center items-center space-y-4">
+    {/* container for scanner limits how big it will grow (large scan portal looks weird) */}
+    <div className="max-w-lg">
+      <Scanner
+      // ignore any barcode format that is not a QR code
+        formats={['qr_code']}
+        allowMultiple={false}
+        onScan={(result: IDetectedBarcode[]): void => {
+          for (const qrCode of result) {
+            console.log(qrCode)
+            console.debug(`Scanned QR: ${qrCode.rawValue}`)
+            if (qrCode.rawValue && validatePassportUrl(qrCode.rawValue)) {
+              const uid = extractUidFromUrl(qrCode.rawValue)
+              console.log(uid)
+              if (uid) {
+              // if scanned QR encodes a valid passport URI/URL, and we can extract the UID, we navigate there
+                router.push(`/passport/${uid}`)
+              } else {
+                setFeedback(`Found component ID ${uid}, but expected something like 'ABC-123456'`)
+              }
+            } else {
+              setFeedback('QR code doesn\'t look like a Material Passport URI 🤔')
+            }
+          }
+        }}
+        onError={console.error}
+      />
+    </div>
+    <p className="text-center">{feedback}</p>
+  </div>)
+}
+
+
